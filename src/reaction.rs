78ef3ffb8172f2e6b849bed3d04ea47334d59a8c
javierg <jgamtejero@gmail.com>
2022-09-26 14:25:39 +0200

    Update simulation and reaction

--- src/reaction.rs
+++ src/reaction.rs
@@ -1,22 +1,39 @@
 struct Reaction {
-    reactants: Vec<String>,
-    products: Vec<String>,
-    st_vec: Vec<i32>
+    reactants: Vec<Particle>,
+    products: Vec<Particle>,
+    st_vec: Vec<i32>,
+    rate: f32
+}
+
+struct Species {
+    name: String,
+    n: f32,
 }
 
 impl Reaction {
-    pub fn new(reactants: Vec<String>, products: Vec<String>) -> Self {
+    pub fn new(reactants: Vec<String>, products: Vec<String>, rate: f32) -> Self {
         let mut st_matrix = [0; reactants.len() + products.len()];
+        let mut reactants_particle = [];
+        let mut products_particle = [];
         for i in 0..reactants.len() {
             st_matrix[i] -= 1;
+            reactants_particle.push(Species {
+                name: reactants[i],
+                n: 0.
+            })
         }
         for i in 0..products.len() {
             st_matrix[reactants.len() + i] += 1;
+            products_particle.push(Species {
+                name: products[i],
+                n: 0.
+            })
         }
         return Reaction {
-            reactants: reactants,
-            products: products,
-            st_matrix: st_matrix
+            reactants: reactants_particle,
+            products: products_particle,
+            st_matrix: st_matrix,
+            rate: rate
         }
     }
 }
---
+++ src/simulation.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+use crate::reaction::{Reaction, Species};
+
+struct Simulation {
+    total_time: f32, // For now, total time and delta are in the same units. TODO: Make a trait with time units
+    delta: f32,
+    species: Vec<Species>,
+    reactions: Vec<Reaction>,
+    propensity_vector: Vec<f32>,
+    total_rate_sum: f32
+}
+
+impl Simulation {
+    fn new() -> Self {
+        return Simulation {
+            reactions: [],
+            propensity_vector: [],
+            total_rate_sum: 0.
+        }
+    }
+
+    fn add_reaction(&mut self, reactants: Vec<String>, products: Vec<String>, rate: f32) {
+        let reaction = Reaction::new(reactants, products, rate);
+        self.reactions.push(reaction);
+        self.propensity_vector.push(0);
+        self.total_rate_sum += rate;
+    }
+
+    fn prepare_propensities(&mut self) {
+        // This is called before starting the simulation
+        for r in 0..self.reactions.len() {
+            if r == 0 {
+                self.propensity_vector[r] /= self.total_rate_sum;
+            } else {
+                self.propensity_vector[r] = (self.propensity_vector[r] + self.propensity_vector[r-1]) / self.total_rate_sum;
+            }
+        }
+        self.propensity_vector.push(1.);
+    }
+
+    fn add_species(&mut self, particle_name: String, particle_number: f32) {
+        self.species.push(Species {
+            name: particle_name,
+            n: particle_number
+        })
+    }
+
+    fn update_propensities(&mut self) {
+
+    }
+}
